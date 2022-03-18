6918f62fc781f685b7ffbdcf2c48e54025451a1f
Paphop Sawasdee <paphop.staw@gmail.com>
2022-03-18 23:56:57 +0700

    Initialized project

---
+++ src/main/java/com/example/cashbox/CashboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashboxApplication.class, args);
+	}
+
+}
