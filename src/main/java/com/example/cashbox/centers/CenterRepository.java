8c52d76fc40c967003f6395915839c0705f5bb4d
Paphop Sawasdee <paphop.staw@gmail.com>
2022-03-19 16:35:44 +0700

    Created CenterRepository #2

---
+++ src/main/java/com/example/cashbox/centers/CenterRepository.java
@@ -0,0 +1,7 @@
+package com.example.cashbox.centers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CenterRepository extends JpaRepository<Center, Integer> {
+
+}
